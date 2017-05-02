a44abd51d92c6c19337bd0371dc134fe0cacbd59
Ashish-Kalra <ashishkalragit1989@gmail.com>
2017-05-03 02:34:31 +0530

    Lazy Initialization: Commit 1

---
+++ LazyInitialization/LazyInitialization/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LazyInitialization
+{
+    class SampleClass
+    {
+        private int mID;
+        private int mAge;
+        private string mName;
+
+        public SampleClass(int id, int age, string name)
+        {
+            mID = id;
+            mAge = age;
+            mName = name;
+        }
+
+        public void ShowDetail()
+        {
+            Console.WriteLine("ID:" + mID + " Age:" + mAge + " Name:" + mName);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Lazy<SampleClass> objSampleClass = new Lazy<SampleClass>(() => new SampleClass(1, 28, "Ashish Kalra"));
+
+            Console.WriteLine("Lazy Initialization of Sample Class");
+
+            objSampleClass.Value.ShowDetail();            
+                        
+            Console.ReadKey();
+        }
+    }
+}
